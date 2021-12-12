9e86f2627d72d4ad59e10f00b81575e928422534
Cris <criscianysilva1997@gmail.com>
2021-12-12 11:55:57 -0400

    Lista de tarefas pronta

---
+++ to-do-list/assets/js/script.js
@@ -0,0 +1,36 @@
+function adicionar() {  
+  let text = document.getElementById('texto').value;
+  let list = document.getElementById('lista').innerHTML;
+
+  list += "<input id='caixa' type='checkbox' onclick='checkbox()' class='marca'>  "+ text + "<br>"
+
+  
+  
+  if(text == ""){
+    alert('Digite sua tarefa!');
+    
+  }else{
+    document.getElementById("lista").innerHTML = list; 
+  }
+  
+}
+//verifica se a checkbox esta marcado ou não
+//caso esteja marcado(checkbox) vai riscar a tarefa, se não remove o risco;
+function checkbox() {
+  let caixa = document.querySelectorAll('input[type=checkbox]');
+  let list = document.getElementById('lista');
+  for(let i = 0;i < caixa.length; i++){
+    if(caixa[i].checked == true){
+      list.style.textDecoration = 'line-through';
+    }else{      
+      list.style.textDecoration = 'none';
+    }
+    
+  }
+  
+}
+
+
+
+
+
